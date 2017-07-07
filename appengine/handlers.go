ab930dff81f7d238408ad11b179ea1fdbadfcaa4
David Sansome <me@davidsansome.com>
2017-07-07 19:31:01 +1000

    Successfully get a certificate

--- appengine/handlers.go
+++ appengine/handlers.go
@@ -4,6 +4,8 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/asn1"
 	"fmt"
 	"io"
 	"net/http"
@@ -14,7 +16,9 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/delay"
 	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/taskqueue"
 	"google.golang.org/appengine/urlfetch"
 	"google.golang.org/appengine/user"
 )
@@ -23,9 +27,9 @@ const (
 	challengePathPrefix   = "/.well-known/acme-challenge/"
 	letsEncryptStagingURL = "https://acme-staging.api.letsencrypt.org/directory"
 
-	registeredAccountKind   = "RegisteredAccount"
+	challengeKind           = "SSLCertificates-Challenge"
+	registeredAccountKind   = "SSLCertificates-RegisteredAccount"
 	registeredAccountIDName = "account"
-	challengeKind           = "Challenge"
 )
 
 type RegisteredAccount struct {
@@ -35,17 +39,31 @@ type RegisteredAccount struct {
 }
 
 type Challenge struct {
-	Type  string
-	URI   string
-	Token string
+	AuthorizationURI string
+	HostName         string
+
+	ChallengeType string
+	ChallengeURI  string
+	Token         string
+	Response      string
 
 	Accepted  time.Time
 	Responded time.Time
-	Response  string
+
+	Error string
+}
+
+func (ch *Challenge) Key(c context.Context) *datastore.Key {
+	return datastore.NewKey(c, challengeKind, ch.Token, 0, nil)
+}
+
+func (ch *Challenge) Put(c context.Context) error {
+	_, err := datastore.Put(c, ch.Key(c), ch)
+	return err
 }
 
 func init() {
-	http.HandleFunc("/auth", wrapHTTPHandler(handleStartAuthorise))
+	http.HandleFunc("/ssl-certificates/auth", wrapHTTPHandler(handleStartAuthorise))
 	http.HandleFunc(challengePathPrefix, wrapHTTPHandler(handleChallenge))
 }
 
@@ -129,8 +147,9 @@ func handleStartAuthorise(c context.Context, w http.ResponseWriter, r *http.Requ
 		return fmt.Errorf("Failed to create ACME client: %v", err)
 	}
 
-	log.Infof(c, "Authorizing %s", r.URL.Host)
-	auth, err := client.Authorize(c, r.URL.Host)
+	hostname := r.URL.Host
+	log.Infof(c, "Authorizing %s", hostname)
+	auth, err := client.Authorize(c, hostname)
 	if err != nil {
 		return fmt.Errorf("Failed to authorize client: %v", err)
 	}
@@ -156,13 +175,15 @@ func handleStartAuthorise(c context.Context, w http.ResponseWriter, r *http.Requ
 		log.Infof(c, "Accepted challenge")
 
 		// Record the challenge and response in datastore.
-		if _, err := datastore.Put(c, datastore.NewKey(c, challengeKind, challenge.Token, 0, nil), &Challenge{
-			Type:     challenge.Type,
-			URI:      challenge.URI,
-			Token:    challenge.Token,
-			Accepted: time.Now(),
-			Response: response,
-		}); err != nil {
+		if err := (&Challenge{
+			AuthorizationURI: auth.URI,
+			HostName:         hostname,
+			ChallengeType:    challenge.Type,
+			ChallengeURI:     challenge.URI,
+			Token:            challenge.Token,
+			Accepted:         time.Now(),
+			Response:         response,
+		}).Put(c); err != nil {
 			return fmt.Errorf("Failed to save challenge: %v", err)
 		}
 		break
@@ -175,15 +196,105 @@ func handleChallenge(c context.Context, w http.ResponseWriter, r *http.Request) 
 
 	// Find the challenge in datastore.
 	entityKey := datastore.NewKey(c, challengeKind, token, 0, nil)
-	challenge := Challenge{}
+	var challenge Challenge
 	if err := datastore.Get(c, entityKey, &challenge); err != nil {
 		return err
 	}
 
-	log.Infof(c, "Responding to challenge %s with %s", challenge.URI, challenge.Response)
+	log.Infof(c, "Responding to challenge %s with %s", challenge.ChallengeURI, challenge.Response)
 	io.WriteString(w, challenge.Response)
 
 	challenge.Responded = time.Now()
-	_, err := datastore.Put(c, entityKey, &challenge)
-	return err
+	if err := challenge.Put(c); err != nil {
+		return fmt.Errorf("Failed to save updated challenge: %v", err)
+	}
+
+	return delayFunc(c, issueCertificateFunc, challenge)
 }
+
+func delayFunc(c context.Context, fn *delay.Function, args ...interface{}) error {
+	task, err := fn.Task(args...)
+	if err != nil {
+		return fmt.Errorf("Failed to create task: %v", err)
+	}
+
+	// Ensure we run on the same module and version.
+	hostname, err := appengine.ModuleHostname(c, "", "", "")
+	if err != nil {
+		return err
+	}
+	task.Header = http.Header{"Host": []string{hostname}}
+
+	// Set some sensible retry options.
+	task.RetryOptions = &taskqueue.RetryOptions{
+		RetryLimit: 10,
+		MinBackoff: 5 * time.Second,
+		MaxBackoff: 30 * time.Second,
+	}
+	task.Delay = 5 * time.Second
+
+	// Schedule the task.
+	task, err = taskqueue.Add(c, task, "")
+	if err != nil {
+		return err
+	}
+	log.Infof(c, "Scheduled task %s", task.Name)
+	return nil
+}
+
+var issueCertificateFunc = delay.Func("issue-certificate", func(c context.Context, ch Challenge) error {
+	client, err := createACMEClient(c)
+	if err != nil {
+		return fmt.Errorf("Failed to create ACME client: %v", err)
+	}
+
+	// Get the status of the challenge.
+	challenge, err := client.GetChallenge(c, ch.ChallengeURI)
+	if err != nil {
+		return fmt.Errorf("Failed to query challenge status: %v", err)
+	}
+	switch challenge.Status {
+	case "pending":
+		return fmt.Errorf("Challenge still pending, will retry later")
+	case "invalid":
+		log.Errorf(c, "Challenge is invalid: %v", challenge.Error)
+		ch.Error = challenge.Error.Error()
+		if err := ch.Put(c); err != nil {
+			log.Errorf(c, "Failed to save challenge: %v", err)
+		}
+		return nil // Don't retry.
+	}
+
+	// Create a new key for this certificate.
+	certKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return fmt.Errorf("Failed to generate RSA private key: %v", err)
+	}
+
+	asn1Subj, _ := asn1.Marshal(pkix.Name{CommonName: ch.HostName}.ToRDNSequence())
+	csr, err := x509.CreateCertificateRequest(rand.Reader, &x509.CertificateRequest{
+		RawSubject:         asn1Subj,
+		SignatureAlgorithm: x509.SHA256WithRSA,
+	}, certKey)
+	if err != nil {
+		return fmt.Errorf("Failed to create CSR: %v", err)
+	}
+
+	ders, url, err := client.CreateCert(c, csr, 90*24*time.Hour, true)
+	if err != nil {
+		return fmt.Errorf("Failed to create certificate: %v", err)
+	}
+	log.Infof(c, "Got %d DER blocks for certificate %s", len(ders), url)
+
+	for _, der := range ders {
+		certs, err := x509.ParseCertificates(der)
+		if err != nil {
+			return fmt.Errorf("Failed to parse certificate: %v", err)
+		}
+
+		for _, cert := range certs {
+			log.Infof(c, "Got cert: %v", cert)
+		}
+	}
+	return nil
+})
